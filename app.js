318c761a3c9d00bce7c84ccf8156390389ab9ce7
Jake1996 <jatinpm96@gmail.com>
2017-05-12 11:30:07 +0530

    app

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ var mongoose = require('mongoose'),
 var books = require('./models/books');
 
 // Connection URL
-var url = 'mongodb://192.168.1.103:27017/bookkart';
+var url = 'mongodb://192.168.1.103:27017/paasdb';
 
 mongoose.connect(url);
 var db = mongoose.connection;
